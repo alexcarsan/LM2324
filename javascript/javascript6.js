48682078f443b5c4f9db5d533371a7121aff8230
Alex Carrero Sánchez <alexcarrero05@iesamachado.org>
2024-01-12 12:52:46 +0100

    terminado

---
+++ javascript/javascript6.js
@@ -0,0 +1,6 @@
+function ocultar(caja){
+    document.getElementById(caja).style.visibility = "hidden";
+}
+function mostrar(caja){
+    document.getElementById(caja).style.visibility = "visible";
+}
